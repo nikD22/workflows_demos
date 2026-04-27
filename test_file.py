c1e895d2d86d639498b4256d5f80e1fecd2734c3
nikD22 <nik.dezelak@student.um.si>
2026-04-27 16:30:31 +0200

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,24 +1,17 @@
-def add(a, b):
-  return a + b
-
-def subtract(a, b):
-  return a - b
-
-def divide(a, b):
-  return a / b
+import main
 
 def test_add():
-  assert add(2, 3) == 5
-  assert add(-1, 1) == 0
-  assert add(0, 0) == 0
+  assert main.add(2, 3) == 5
+  assert main.add(-1, 1) == 0
+  assert main.add(0, 0) == 0
 
 
 def test_subtract():
-  assert subtract(5, 3) == 2
-  assert subtract(0, 4) == -4
-  assert subtract(-2, -2) == 0
+  assert main.subtract(5, 3) == 2
+  assert main.subtract(0, 4) == -4
+  assert main.subtract(-2, -2) == 0
 
 
 def test_divide():
-  assert divide(6, 3) == 2
-  assert divide(5, 2) == 2.5
+  assert main.divide(6, 3) == 2
+  assert main.divide(5, 2) == 2.5
