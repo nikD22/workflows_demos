94513adc3f21668bf3925ba11bfc50a073c7296d
nikD22 <nik.dezelak@student.um.si>
2026-04-27 16:29:17 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def divide(a, b):
+    return a / b
+
+
+# Optional: simple manual run
+if __name__ == "__main__":
+    print("hello world")
